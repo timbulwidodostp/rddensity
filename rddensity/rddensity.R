c8b2cc23dcfb65dc00eab6c084f0f8d7835e2476
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-04-01 10:01:36 +0700

    Use rddensity With (In) R Software

---
+++ rddensity/rddensity.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Manipulation testing using local polynomial density estimation Use rddensity With (In) R Software
+install.packages("rddensity")
+library("rddensity")
+spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/rddensity/main/rddensity/rddensity.csv",sep = ";")
+# Estimation Manipulation testing using local polynomial density estimation Use rddensity With (In) R Software
+x <- rddensity$rddensity
+rddensity <- rddensity(X = x, vce = "jackknife")
+summary(rddensity)
+# Manipulation testing using local polynomial density estimation Use rddensity With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
